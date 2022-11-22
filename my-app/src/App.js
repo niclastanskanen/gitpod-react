89f2dc144a1488fd5556b0f808abfc544d0c9a39
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-22 13:20:49 +0000

    add info to greeting with props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
+// import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalGreeting />
-      <FunctionalGreetingWithProps />
+      {/* <FunctionalGreeting /> */}
+      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike!" age="32" />
     </div>
   );
 }
