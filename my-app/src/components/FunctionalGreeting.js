c5cfb75c4ee9bf5f3a7c09f9b67307ebfeedad4f
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-22 13:13:25 +0000

    add functional with without props

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// function FunctionlaGreeting() {
+//     return <h1>Hello from React!</h1>
+// }
+
+const FunctionalGreeting = () => <h1>Hello from React!</h1>;
+
+export default FunctionalGreeting;
