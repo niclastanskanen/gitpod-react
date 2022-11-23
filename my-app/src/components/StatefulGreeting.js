ef4b7a98d8e90882a45b2c191d2e2008e6f865ff
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-23 12:05:39 +0000

    add this state

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,10 +1,33 @@
 import React from "react"
 
+// class StatefulGreeting extends React.Component {
+//     render() {
+//         // return <h1>Heey from StatefulGreeting</h1>
+//         return <h1>Hello {this.props.greeting}</h1>
+//     }
+// }
+
+// export default StatefulGreeting;
+
 class StatefulGreeting extends React.Component {
-    render() {
-        // return <h1>Heey from StatefulGreeting</h1>
-        return <h1>Hello {this.props.greeting}</h1>
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
     }
+
+    render() {
+        return(
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                <button>{this.state.buttonText}</button>
+            </div>
+        )
+    }
+    
 }
 
 export default StatefulGreeting;
