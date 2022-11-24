8fb3ec09f1824b63c59b15cfba95bd0788763b74
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-24 08:48:32 +0000

    add es7 snippets

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -1,43 +1,43 @@
-// import React from "react"
-
-// // class StatefulGreeting extends React.Component {
-// //     render() {
-// //         // return <h1>Heey from StatefulGreeting</h1>
-// //         return <h1>Hello {this.props.greeting}</h1>
-// //     }
-// // }
-
-// // export default StatefulGreeting;
+import React from "react"
 
 // class StatefulGreeting extends React.Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             introduction: "Hello!",
-//             buttonText: "Exit",
-//         };
-//     }
-
-
-//     handleClick() {
-//         this.setState({
-//             introduction: "Goodbye!",
-//             buttonText: "Enter",
-//         });
-//         console.log(this.state.introduction);
-//     }
-
-
 //     render() {
-//         return(
-//             <div>
-//                 <h1>{this.state.introduction} {this.props.greeting}</h1>
-//                 <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
-//             </div>
-//         )
+//         // return <h1>Heey from StatefulGreeting</h1>
+//         return <h1>Hello {this.props.greeting}</h1>
 //     }
-    
 // }
 
 // export default StatefulGreeting;
+
+class StatefulGreeting extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            introduction: "Hello!",
+            buttonText: "Exit",
+        };
+    }
+
+
+    handleClick() {
+        this.setState({
+            introduction: "Goodbye!",
+            buttonText: "Enter",
+        });
+        console.log(this.state.introduction);
+    }
+
+
+    render() {
+        return(
+            <div>
+                <h1>{this.state.introduction} {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+        )
+    }
+    
+}
+
+export default StatefulGreeting;
