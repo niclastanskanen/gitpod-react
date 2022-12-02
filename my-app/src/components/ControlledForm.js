ec86ab5dbd9fec29935daa034717593a2ae5efe3
niclastanskanen <niclastanskanen@hotmail.com>
2022-12-02 09:12:18 +0000

    add handleSubmit

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -3,34 +3,74 @@ import React, { Component } from 'react'
 export class ControlledForm extends Component {
 
     constructor(props) {
-      super(props)
-    
-      this.state = {
-        name: ''
-      }
+        super(props)
+
+        this.state = {
+            name: '',
+            category: 'website',
+            comments: '',
+        }
     }
     handleNameChange = (event) => {
         this.setState({
             name: event.target.value
         })
     }
+    handleCategoryChange = (event) => {
+        this.setState({
+            category: event.target.value
+        })
+    }
+    handleComments = (event) => {
+        this.setState({
+            comments: event.target.value
+        })
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+    }
 
-  render() {
-    return (
-      <div>
-        <form>
-            <label htmlFor="id-name">Your Name:</label>
-            <input 
-                value={this.state.name}
-                onChange={this.handleNameChange}
-                id="id-name"
-                name="name" type="text"
-            />
-            <input type="submit" value="Submit" />
-        </form>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h2>Please fill out the form below:</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor="id-name">Your Name:</label>
+                        <input
+                            value={this.state.name}
+                            onChange={this.handleNameChange}
+                            id="id-name"
+                            name="name" type="text"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="id-category">Query category:</label>
+                        <select 
+                            id="id-category"
+                            name="category"
+                            value={this.state.category}
+                            onChange={this.handleCategoryChange}
+                        >
+                            <option value="website">Website issue</option>
+                            <option value="order">Order issue</option>
+                            <option value="general">General inquiry</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="id-comments">Comments:</label>
+                        <textarea 
+                            id="id-comments"
+                            name="comments"
+                            value={this.state.comments}
+                            onChange={this.handleComments}
+                        />
+                    </div>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
 }
 
 export default ControlledForm
