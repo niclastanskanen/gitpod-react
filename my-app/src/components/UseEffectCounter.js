f33c30863df5435691a397d65a9fd4a49e64745e
niclastanskanen <niclastanskanen@hotmail.com>
2022-12-06 09:01:44 +0000

    restrict use effect hook

--- my-app/src/components/UseEffectCounter.js
+++ my-app/src/components/UseEffectCounter.js
@@ -4,13 +4,23 @@ function UseEffectCounter() {
     const [count, setCount] = useState(0);
     useEffect(() => {
         document.title = count;
-    });
+    }, [count]);
+
+    const [count2, setCount2] = useState(0);
+    useEffect(() => {
+        document.title = count2;
+    }, [count2]);
   return (
     <div>
         <button onClick={
             () => setCount(count => count + 1)
         }>
             Increment Count ({count})
+        </button>
+        <button onClick={
+            () => setCount2(count2 => count2 + 1)
+        }>
+            Increment Count2 ({count2})
         </button>
     </div>
   )
