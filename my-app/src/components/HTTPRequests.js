645b2becd527ee8cfbc3a38075ab925dc41e5980
niclastanskanen <niclastanskanen@hotmail.com>
2022-12-06 12:02:46 +0000

    API response objevt and array

--- my-app/src/components/HTTPRequests.js
+++ my-app/src/components/HTTPRequests.js
@@ -7,22 +7,46 @@ export class HTTPRequests extends Component {
     
       this.state = {
          posts: [],
+         error: null,
       }
     }
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
         .then(response => {
             this.setState({
-                posts: response.data
+                posts: Array.isArray(response.data)
+                ? response.data
+                : [response.data]
+            })
+        })
+        .catch(error => {
+            this.setState({
+                error: error.message
             })
         })
     }
 
   render() {
+    const posts = this.state.posts;
     return (
       <div>
         <h2>Posts: </h2>
-        {JSON.stringify(this.state.posts)}
+        {
+            posts.length ? (
+                posts.map(post => (
+                    <div key={post.id}>
+                        <h2>{post.id}. {post.title}</h2>
+                        <h4>By User ID {post.userID}</h4>
+                        <p>{post.body}</p>
+                        <hr />
+                    </div>
+                ))
+            ) : (
+                this.state.error
+                ? <p>{this.state.error}</p>
+                : <h4>Loading posts ...</h4>
+            )
+        }
       </div>
     )
   }
